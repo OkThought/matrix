5582d055e4a5abc00e14218aa4021779f75687b7
RoyPy <bogush.vano@gmail.com>
2018-08-27 11:28:56 +0700

    Make crossed out buttons disabled

--- src/board.tsx
+++ src/board.tsx
@@ -18,19 +18,21 @@ class Board extends React.Component<Interface> {
             <div className="row" key={rowIndex}>
               {row.map((theNumber, colIndex) => {
                 let buttonClasses = "number btn btn-dark btn-sm rounded-0";
-
+                let buttonDisabled = false;
                 if (rowSelected &&
                   colIndex === this.props.selectedNumberIndex.col) {
                   buttonClasses += " selected";
                 } else if (theNumber === 0) {
                   buttonClasses += " crossed-out";
+                  buttonDisabled = true;
                 }
 
                 return (
                   <div className="col-" key={colIndex}>
                     <button type="button"
                             className={buttonClasses}
-                            onClick={() => this.props.onNumberClick(rowIndex, colIndex)}>
+                            onClick={() => this.props.onNumberClick(rowIndex, colIndex)}
+                            disabled={buttonDisabled}>
                       {theNumber}
                     </button>
                   </div>
