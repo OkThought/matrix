3caf7fc67c3ae0e9622bd95ae2a86c9c32e35372
RoyPy <bogush.vano@gmail.com>
2018-08-28 12:26:18 +0700

    Include styles into build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app';
+// noinspection ES6UnusedImports
+import './styles';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
