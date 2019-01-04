6be1044e82dcd19dfdd15f7b2e3c40ac0dd9fd76
OkThought <bogush.vano@gmail.com>
2019-01-04 23:27:10 +0700

    Merge router into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as styles from './index.module.css';
-import AppRouter from "./router/router";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import Matrix from "./game/matrix";
+import Home from "./home/home";
+import Rules from "./rules/rules";
+import * as styles from './styles.css';
 
 const root = document.getElementById('root');
 const Index = () => (
-  <div className={styles.index}>
-    <AppRouter/>
+  <div className={styles.Index}>
+    <Router basename="matrix">
+      <div className="container">
+        <div className="row">
+          <div className="col-">
+            <nav className="navbar collapse navbar-collapse navbar-light justify-content-md-center">
+              <ul className="navbar-nav flex-column">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/game/">Play</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/rules/">Rules</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-sm">
+            <main role="main" className="container">
+              <Route path="/" exact component={Home}/>
+              <Route path="/game/" exact component={Matrix}/>
+              <Route path="/rules/" exact component={Rules}/>
+            </main>
+          </div>
+        </div>
+      </div>
+    </Router>
   </div>
 );
 
