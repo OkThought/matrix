0acbe6c1c49450840481a0642e372fd21e51baf3
OkThought <bogush.vano@gmail.com>
2019-01-04 16:41:22 +0700

    Implement vertical nav bar on the left side using bootstrap

--- src/router.tsx
+++ src/router.tsx
@@ -6,25 +6,31 @@ import Rules from "./rules/rules";
 
 const AppRouter = () => (
   <Router basename="matrix">
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/game/">Play</Link>
-          </li>
-          <li>
-            <Link to="/rules/">Game Rules</Link>
-          </li>
-        </ul>
-      </nav>
-      <main role="main" className="container">
-        <Route path="/" exact component={Home}/>
-        <Route path="/game/" exact component={Matrix}/>
-        <Route path="/rules/" exact component={Rules}/>
-      </main>
+    <div className="container">
+      <div className="row">
+        <div className="col-xsm">
+          <nav className="navbar collapse navbar-collapse navbar-light justify-content-md-center">
+            <ul className="navbar-nav flex-column">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/game/">Play</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/rules/">Rules</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="col-sm">
+          <main role="main" className="container">
+            <Route path="/" exact component={Home}/>
+            <Route path="/game/" exact component={Matrix}/>
+            <Route path="/rules/" exact component={Rules}/>
+          </main>
+        </div>
+      </div>
     </div>
   </Router>
 );
