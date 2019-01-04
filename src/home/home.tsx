ba31da864d7042f4b9b1593825cdbe9577b55738
OkThought <bogush.vano@gmail.com>
2019-01-04 13:05:33 +0700

    Add home page

---
+++ src/home/home.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Home = () => (
+  <div>
+    This is a home page :)
+  </div>
+);
+
+export default Home;
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import Matrix from "./game/matrix";
+import Home from "./home/home";
 
 const AppRouter = () => (
   <Router>
@@ -8,12 +9,16 @@ const AppRouter = () => (
       <nav>
         <ul>
           <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/game/">Play</Link>
           </li>
         </ul>
       </nav>
       <main role="main" className="container">
-        <Route path="/game/" exact component={Matrix} />
+        <Route path="/" exact component={Home}/>
+        <Route path="/game/" exact component={Matrix}/>
       </main>
     </div>
   </Router>
