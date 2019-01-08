4e180bbde5ab6ca3b42ef3b0ece9b8077adcd674
OkThought <bogush.vano@gmail.com>
2019-01-08 23:59:59 +0700

    Updates

---
+++ precache-manifest.ae98d6703b571c106a604ea18ddee544.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "40baaec549e42ce31f2a61b10fb2dfa3",
+    "url": "/matrix/static/media/cross_out_horizontal_with_zeros.40baaec5.png"
+  },
+  {
+    "revision": "89a49d4062f59a5f4834",
+    "url": "/matrix/static/css/main.01b2bbff.chunk.css"
+  },
+  {
+    "revision": "65b74a243fbd49313fbe",
+    "url": "/matrix/static/js/1.65b74a24.chunk.js"
+  },
+  {
+    "revision": "6d9a45bfd2187f3a91a4",
+    "url": "/matrix/static/js/runtime~main.6d9a45bf.js"
+  },
+  {
+    "revision": "55e6cbd909be50038e931ad1caf72e72",
+    "url": "/matrix/static/media/classical_board.55e6cbd9.png"
+  },
+  {
+    "revision": "bdebbf05ad0fdb838fd4eb802305636b",
+    "url": "/matrix/static/media/cross_out_horizontal.bdebbf05.png"
+  },
+  {
+    "revision": "89a49d4062f59a5f4834",
+    "url": "/matrix/static/js/main.89a49d40.chunk.js"
+  },
+  {
+    "revision": "121655423ab6010d238fc1e88a5d74b9",
+    "url": "/matrix/static/media/cross_out_horizontal_with_zeros_next_line.12165542.png"
+  },
+  {
+    "revision": "70338c3ccb2e812ec208c23c0b22c838",
+    "url": "/matrix/static/media/cross_out_vertical.70338c3c.png"
+  },
+  {
+    "revision": "7fd132f5f975cc92e2085aa68e43960e",
+    "url": "/matrix/static/media/cross_out_vertical_with_zeros.7fd132f5.png"
+  },
+  {
+    "revision": "11064122491926599d5abd4e9a15d3dc",
+    "url": "/matrix/static/media/next_level.11064122.png"
+  },
+  {
+    "revision": "5c9dd8a6b20f8a0f017536e4ea70980e",
+    "url": "/matrix/index.html"
+  }
+];
