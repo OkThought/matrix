1416e3a59aaa140ea5fdac902288126b761edd38
OkThought <bogush.vano@gmail.com>
2019-01-08 23:54:06 +0700

    Add navbar toggler

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,17 @@ const Index = () => (
   <BrowserRouter basename="matrix">
     <div className='Index h-100'>
       <nav className="navbar sticky-top navbar-expand-sm navbar-dark bg-dark">
-        <nav className="container navbar-nav justify-content-start">
-          <Link className="navbar-brand nav-item nav-link" to="/">Matrix</Link>
-          <Link className="nav-item nav-link" to="/game/">Play</Link>
-          <Link className="nav-item nav-link" to="/rules/">Rules</Link>
-        </nav>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"/>
+        </button>
+        <div className="collapse navbar-collapse" id="main-nav">
+          <nav className="container navbar-nav justify-content-start">
+            <Link className="navbar-brand nav-item nav-link" to="/">Matrix</Link>
+            <Link className="nav-item nav-link" to="/game/">Play</Link>
+            <Link className="nav-item nav-link" to="/rules/">Rules</Link>
+          </nav>
+        </div>
       </nav>
       <div role="main" className="container">
         <Route path="/" exact component={Home}/>
